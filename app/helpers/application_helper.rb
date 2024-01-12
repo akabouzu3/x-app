cd3fd25ef8ad84c1735cd26a847d714761bbcc07
kazuo <kazuo@example.com>
2024-01-12 10:11:46 +0000

    hello

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "kazuo_twitter"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def embedded_svg(filename, options = {})
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    svg['class'] = options[:class] if options[:class].present?
+    doc.to_html.html_safe
+  end
 end
